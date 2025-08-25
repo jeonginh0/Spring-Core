27918df2825750c32c26878b89b7d1d8d1b884ca
정인호 <jeonginho@jeong-inhoui-MacBookAir.local>
2025-08-25 15:51:28 +0900

    Add: 2025-08-25, 4장 학습 완료

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -8,23 +8,30 @@ import com.example.springcore.member.MemberServiceImpl;
 import com.example.springcore.member.MemoryMemberRepository;
 import com.example.springcore.order.OrderService;
 import com.example.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 멤버
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 할인 정책 적용
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/com/example/springcore/MemberApp.java
+++ src/main/java/com/example/springcore/MemberApp.java
@@ -4,11 +4,17 @@ import com.example.springcore.member.Grade;
 import com.example.springcore.member.Member;
 import com.example.springcore.member.MemberService;
 import com.example.springcore.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
